b1c7292278fe1b12807cfb68c34e406ab013ddce
Scherbi <shherbakov99ilya@mail.ru>
2023-01-15 17:38:48 +0300

    Initial commit

---
+++ src/test/java/sky/pro/SkyDreamTeam/AnimalService/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.SkyDreamTeam.AnimalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
