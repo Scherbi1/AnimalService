5489655cee8774b29ea73aafa8172f3c90e5acda
Alexander <emotionless013@mail.ru>
2023-01-28 18:05:11 +0400

    Создал класс InformationLoader, загружает инфу из текстовых файлов в БД. В модели Information удалил числовое поле ID, достаточно наименования файла и содержания.

--- src/main/java/sky/pro/SkyDreamTeam/AnimalService/service/BotUpdatesListener.java
+++ src/main/java/sky/pro/SkyDreamTeam/AnimalService/service/BotUpdatesListener.java
@@ -1,13 +1,8 @@
 package sky.pro.SkyDreamTeam.AnimalService.service;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.File;
-import com.pengrad.telegrambot.model.PhotoSize;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.GetFile;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.request.SendPhoto;
-import com.pengrad.telegrambot.response.GetFileResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +12,8 @@ import sky.pro.SkyDreamTeam.AnimalService.initialization.InitData;
 import sky.pro.SkyDreamTeam.AnimalService.model.BotMenu;
 
 import javax.annotation.PostConstruct;
-
-import java.io.BufferedInputStream;
-
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-
 
 import static sky.pro.SkyDreamTeam.AnimalService.model.BotMenu.*;
 
@@ -39,7 +27,6 @@ public class BotUpdatesListener implements UpdatesListener {
     @Autowired
     private TelegramBot telegramBot;
 
-    private List<File> fileArchive=new ArrayList<>();
 
     @PostConstruct
     public void init() {
@@ -59,13 +46,6 @@ public class BotUpdatesListener implements UpdatesListener {
                     break;
                 case INFO:
                     infoMenu(update);
-                    break;
-                case REPORT:
-                    try {
-                        reportMenu(update);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
                     break;
                 default:
                     sendReportMassage(chatId);
@@ -88,9 +68,6 @@ public class BotUpdatesListener implements UpdatesListener {
             if (updateMassage.equals("/info")) {
                 sendInfoMassage(chatId);
             }
-            if (updateMassage.equals("/report")) {
-                sendReportMassage(chatId);
-            }
 
         }
 
@@ -100,9 +77,8 @@ public class BotUpdatesListener implements UpdatesListener {
     private void sendStartMassage(long chatId) {
         String massage = "Этот телеграм бот обслуживает сервис Приюта для Животных" + "\n" +
                 "Команды:" + "\n" +
-                "Главное меню /start" + "\n" +
-                "Информационное меню /info"+ "\n" +
-                "Отчет /report";
+                "Главное меню: /start" + "\n" +
+                "Информационное меню /info";
         SendMessage message = new SendMessage(chatId, massage);
         telegramBot.execute(message);
         botMenu = START;
@@ -145,69 +121,10 @@ public class BotUpdatesListener implements UpdatesListener {
         logger.info("Send Contact massage to chatId: {}", chatId);
     }
 
-    private void reportMenu(Update update) throws IOException {
-        long chatId = (update.message().chat().id());
-        String updateMassage = update.message().text();
-        if (updateMassage != null) {
-            if (updateMassage.equals("/start")) {
-                sendStartMassage(chatId);
-            }
-        }
-        if (update.message().photo() != null) {
-            System.out.println("Foto load");
-
-            PhotoSize[] photo = update.message().photo();
-
-            String fileId =photo[photo.length-1].fileId();
-            GetFile request = new GetFile(fileId);
-            GetFileResponse getFileResponse = telegramBot.execute(request);
-
-            File file = getFileResponse.file();
-            file.fileId();
-            file.filePath();
-            file.fileSize();
-
-            fileArchive.add(file);
-            System.out.println(file.filePath());
-            fileArchive.stream().forEach(f->{
-                SendPhoto sendPhoto = new SendPhoto(chatId,f.fileId());
-                telegramBot.execute(sendPhoto);
-            });
-
-            String fullPath = telegramBot.getFullFilePath(file);
-
-            String filePathName="src/main/"+file.filePath();
-
-            try (BufferedInputStream in = new BufferedInputStream(new URL(fullPath).openStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream(filePathName)) {
-                byte dataBuffer[] = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                    fileOutputStream.write(dataBuffer, 0, bytesRead);
-                }
-            } catch (IOException e) {
-                System.out.println("Не получилось");
-            }
-        }
-
-        logger.info("Processing reportMenu: {}", fileArchive);
-    }
-
-    private void sendReportMassage(long chatId) {
-        String massage = "Отчет" + "\n" +
-                "Команды:" + "\n" +
-                "Главное меню /start" + "\n"+
-                "Прикрепите Фотографию.";
-        SendMessage message = new SendMessage(chatId, massage);
-        telegramBot.execute(message);
-        botMenu = REPORT;
-        logger.info("Send Start massage to chatId: {}", chatId);
-    }
 
     @Bean(initMethod = "runAfterObjectCreated")
     public InitData runAfterObjectCreated() {
         return new InitData();
     }
-
 
 }
