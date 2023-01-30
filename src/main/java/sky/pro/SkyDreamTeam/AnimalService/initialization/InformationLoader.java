dc8c741ed399bef5f638062b70576e768e6d53bd
Ainur <ainurforex@mail.ru>
2023-01-30 19:50:38 +0500

    Доделал архитектуру, добавил новые классы

--- src/main/java/sky/pro/SkyDreamTeam/AnimalService/initialization/InformationLoader.java
+++ src/main/java/sky/pro/SkyDreamTeam/AnimalService/initialization/InformationLoader.java
@@ -2,6 +2,8 @@ package sky.pro.SkyDreamTeam.AnimalService.initialization;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import sky.pro.SkyDreamTeam.AnimalService.model.Information;
 import sky.pro.SkyDreamTeam.AnimalService.repository.InformationRepository;
@@ -12,9 +14,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import static sky.pro.SkyDreamTeam.AnimalService.initialization.InitData.readUsingFiles;
+import static sky.pro.SkyDreamTeam.AnimalService.utils.FileUtil.removeFileExtension;
+
 @Service
 public class InformationLoader {
-    InformationRepository informationRepository;
+    private final InformationRepository informationRepository;
 
     public InformationLoader(InformationRepository informationRepository) {
         this.informationRepository = informationRepository;
@@ -22,34 +27,30 @@ public class InformationLoader {
 
     Logger logger = LoggerFactory.getLogger(InformationLoader.class);
 
+
+    private String loadContent="true";
+
     public void loadInfo() throws IOException {
-        logger.info("Загрузка информационных текстов из фйлов каталога information.data");
 
-        File folder = new File("src/main/informationData");
-        File[] listOfFiles = folder.listFiles();
+        if (loadContent.equals("true")) {
+            logger.info("Загрузка information.data loadContentInStart=true");
+            File folder = new File("src/main/informationData");
+            File[] listOfFiles = folder.listFiles();
 
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
- //               BasicFileAttributes attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
- //               if (attributes.lastModifiedTime())
-                String key = removeFileExtension(file.getName()).toLowerCase();
-                String content = readUsingFiles(folder + "/" + file.getName());
-                Information information = new Information(key, content);
-                informationRepository.save(information);
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+
+                    String key = removeFileExtension(file.getName()).toLowerCase();
+                    String content = readUsingFiles(folder + "/" + file.getName());
+                    Information information = new Information(key, content);
+                    informationRepository.save(information);
+                }
             }
+        } else {
+            logger.info("information.data не будет загружена loadContentInStart=false");
         }
-
-    }
-    private static String readUsingFiles(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 
-    public static String removeFileExtension(String filename) {
-        if (filename == null || filename.isEmpty()) {
-            return filename;
-        }
 
-        String extPattern = "(?<!^)[.]" + ".*";
-        return filename.replaceAll(extPattern, "");
-    }
+
 }
--- src/main/java/sky/pro/SkyDreamTeam/AnimalService/repository/PersonRepository.java
+++ src/main/java/sky/pro/SkyDreamTeam/AnimalService/repository/PersonRepository.java
@@ -1,7 +1,21 @@
 package sky.pro.SkyDreamTeam.AnimalService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import sky.pro.SkyDreamTeam.AnimalService.model.BotMenu;
 import sky.pro.SkyDreamTeam.AnimalService.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Person findById(long id);
+
+    Person findByChatId(long id);
+
+    @Query(value = "SELECT bot_menu FROM person where chat_id=:ChatId", nativeQuery = true)
+    BotMenu getBotMenuByChatId(long ChatId);
+//Заставить работать что быбыстрее было
+//    @Query(value = "UPDATE person SET bot_menu=:menuNumber WHERE chat_id=:ChatId", nativeQuery = true)
+//    BotMenu setBotMenuByChatId(long ChatId,int menuNumber);
+
+
 }
