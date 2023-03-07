e045bafb9d0d2fcafdf7b5939e9906d7f99d640d
Alexander <emotionless013@mail.ru>
2023-03-07 18:59:40 +0400

    Добавил комментарии к сервисам моделей Pet.

--- src/main/java/sky/pro/SkyDreamTeam/AnimalService/service/DogShelter/DogShelterPetService.java
+++ src/main/java/sky/pro/SkyDreamTeam/AnimalService/service/DogShelter/DogShelterPetService.java
@@ -2,6 +2,7 @@ package sky.pro.SkyDreamTeam.AnimalService.service.DogShelter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import sky.pro.SkyDreamTeam.AnimalService.exceptions.PetNotFoundException;
 import sky.pro.SkyDreamTeam.AnimalService.model.DogShelter.DogShelterPet;
@@ -20,25 +21,58 @@ public class DogShelterPetService {
 
     Logger logger = LoggerFactory.getLogger(DogShelterPetService.class);
 
+    /**
+     * Этот метод создает нового питомца в базе данных собак.
+     * <br> Используется метод репозитория {@link JpaRepository#save(Object)}
+     *
+     * @param pet данные о животном в формате JSON
+     * @return данные о животном в формате JSON, которые были сохранены в БД
+     */
     public DogShelterPet createPet(DogShelterPet pet) {
         logger.info("Dog shelter Was invoked method for createPet");
         return petRepository.save(pet);
     }
 
+    /**
+     * Этот метод редактирует запись о питомце в базе данных собак.
+     * <br> Используется метод репозитория {@link JpaRepository#save(Object)}
+     *
+     * @param pet данные о животном в формате JSON
+     * @return данные о животном в формате JSON, которые были сохранены в БД
+     */
     public DogShelterPet editPet(DogShelterPet pet) {
         return petRepository.save(pet);
     }
 
+    /**
+     * Этот метод позволяет найти запись о питомце в базе данных собак по имени.
+     * <br> Используется метод репозитория {@link DogShelterPetRepository#findDogShelterPetByName(String)}
+     *
+     * @param name имя собаки
+     * @return данные о животном в формате JSON, которые были сохранены в БД
+     */
     public DogShelterPet findPetByName(String name) {
         logger.info("Dog shelter Was invoked method for findPerson");
         return petRepository.findDogShelterPetByName(name).orElseThrow(PetNotFoundException::new);
     }
 
+    /**
+     * Этот метод удаляет запись о питомце в базе данных собак по ID.
+     * <br> Используется метод репозитория {@link DogShelterPetRepository#deleteById(Object)}
+     *
+     * @param id идентификатор записи о собаке в базе данных
+     */
     public void deletePet(long id) {
         logger.info("Dog shelter Was invoked method for deletePet");
         petRepository.deleteById(id);
     }
 
+    /**
+     * Этот метод удаляет запись о питомце в базе данных собак по имени.
+     * <br> Используется метод репозитория {@link DogShelterPetRepository#removeByName(String)}
+     *
+     * @param name имя собаки
+     */
     public void deletePet(String name) {
         logger.info("Dog shelter Was invoked method for deletePet");
         petRepository.removeByName(name);
