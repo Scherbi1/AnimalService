af54e0e57f76b530ff0c6d2c850bb93419efd483
Alexander <emotionless013@mail.ru>
2023-03-08 09:00:46 +0400

    Добавил контроллер/сервис/модель взаимодействий.

---
+++ src/main/java/sky/pro/SkyDreamTeam/AnimalService/repository/InteractionsAdminAndClientRepository.java
@@ -0,0 +1,7 @@
+package sky.pro.SkyDreamTeam.AnimalService.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sky.pro.SkyDreamTeam.AnimalService.model.InteractionsAdminAndClient;
+
+public interface InteractionsAdminAndClientRepository extends JpaRepository<InteractionsAdminAndClient,Long> {
+}
+
